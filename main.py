d41071118b10033379e6586ce4cae6a61584ad05
skinact123 <arvin.a@skinact.com>
2025-04-28 10:42:53 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,18 +2,28 @@ from fastapi import FastAPI, Request
 from symspellpy.symspellpy import SymSpell, Verbosity
 
 app = FastAPI()
+
 sym_spell = SymSpell(max_dictionary_edit_distance=2)
 sym_spell.load_dictionary("frequency_dictionary_en_82_765.txt", term_index=0, count_index=1)
 
+VERIFICATION_TOKEN = "spaandequipment123"  # you can set any token you like
+
+@app.get("/verify")
+async def verify(verification_token: str):
+    if verification_token == VERIFICATION_TOKEN:
+        return {"status": "ok"}
+    else:
+        return {"status": "error"}
+
 @app.get("/")
 def read_root():
-    return {"status": "Spellcheck API is running!"}
+    return {"status": "spellcheck API is running!"}
 
 @app.post("/")
 async def correct_spelling(req: Request):
     try:
         data = await req.json()
-        user_input = data.get("message", "")
+        user_input = data.get("Message", "")
         suggestions = sym_spell.lookup(user_input, Verbosity.CLOSEST, max_edit_distance=2)
         corrected = suggestions[0].term if suggestions else user_input
         return {"message": corrected}
