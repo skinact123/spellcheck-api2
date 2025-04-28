7cdd616d00e26061de1b8bcbd2f24bd8f1aa1220
skinact123 <arvin.a@skinact.com>
2025-04-28 10:56:34 -0700

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 sym_spell = SymSpell(max_dictionary_edit_distance=2)
 sym_spell.load_dictionary("frequency_dictionary_en_82_765.txt", term_index=0, count_index=1)
 
-VERIFICATION_TOKEN = "spaandequipment123"  # <-- you can set this anything you want
+VERIFICATION_TOKEN = "spaandequipment123"
 
 @app.get("/verify")
 async def verify(verification_token: str):
@@ -23,7 +23,7 @@ def read_root():
 async def correct_spelling(req: Request):
     try:
         data = await req.json()
-        user_input = data.get("Message", "")
+        user_input = data.get("message", "")
         suggestions = sym_spell.lookup(user_input, Verbosity.CLOSEST, max_edit_distance=2)
         corrected = suggestions[0].term if suggestions else user_input
         return {"message": corrected}
